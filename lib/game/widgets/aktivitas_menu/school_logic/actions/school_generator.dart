42956d7cb740dac9b52d5e1cb3753d25a1f18d25
Muhfahmm <fahimfahim0407@gmail.com>
2026-08-20 18:28:47 +0700

    update

--- lib/game/widgets/aktivitas_menu/school_logic/actions/school_generator.dart
+++ lib/game/widgets/aktivitas_menu/school_logic/actions/school_generator.dart
@@ -1,56 +1,19 @@
-// lib/game/widgets/aktivitas_menu/school_logic/actions/school_generator.dart
-
+import 'package:bitlife/game/widgets/hubungan_menu/school_sexuality/school_sexuality_logic.dart';
 import 'package:bitlife/pilih_karakter/character.dart';
 import 'dart:math';
 
 class SchoolGenerator {
   static final Random _random = Random();
 
-  static final List<String> maleNames = [
-    'Aditya', 'Budi', 'Candra', 'Dedi', 'Eko', 'Fajar', 'Guntur', 'Hendra', 'Irfan', 'Joko',
-    'Kurniawan', 'Lukman', 'Maman', 'Nugroho', 'Oki', 'Prabowo', 'Rian', 'Suryo', 'Taufik', 'Umar',
-    'Wawan', 'Yanto', 'Zainal', 'Rian', 'Dimas', 'Angga', 'Rendi', 'Bagus', 'Arief', 'Rizal'
-  ];
-
-  static final List<String> femaleNames = [
-    'Anisa', 'Bunga', 'Citra', 'Dewi', 'Elisa', 'Fitri', 'Gita', 'Hani', 'Indah', 'Julia',
-    'Kartika', 'Laras', 'Melati', 'Novi', 'Olivia', 'Putri', 'Ratih', 'Siti', 'Tari', 'Utami',
-    'Wulan', 'Yani', 'Zahra', 'Rina', 'Sari', 'Mega', 'Santi', 'Dian', 'Ayu', 'Nining'
-  ];
-
-  static final List<String> lastNames = [
-    'Saputro', 'Wijaya', 'Sihombing', 'Pratama', 'Hidayat', 'Kusuma', 'Santoso', 'Gunawan', 'Siregar', 'Lubis',
-    'Nasution', 'Simanjuntak', 'Setiawan', 'Budiman', 'Wibowo', 'Nugraha', 'Harahap', 'Ginting', 'Sutrisno', 'Purnama'
-  ];
-
   static String generateRandomName(String gender, Character character) {
-    List<String> firstList;
-    List<String> lastList;
-    
-    final bool useDynamic = gender == 'Laki-laki' 
-        ? (character.maleFirstNames != null && character.maleFirstNames!.isNotEmpty)
-        : (character.femaleFirstNames != null && character.femaleFirstNames!.isNotEmpty);
-
-    if (useDynamic) {
-      firstList = gender == 'Laki-laki' ? character.maleFirstNames! : character.femaleFirstNames!;
-      lastList = (character.lastNames != null && character.lastNames!.isNotEmpty) 
-          ? character.lastNames! 
-          : lastNames;
-    } else {
-      firstList = gender == 'Laki-laki' ? maleNames : femaleNames;
-      lastList = lastNames;
-    }
+    List<String> firstList = gender == 'Laki-laki' 
+        ? (character.maleFirstNames ?? []) 
+        : (character.femaleFirstNames ?? []);
+    List<String> lastList = character.lastNames ?? [];
 
     final first = firstList[_random.nextInt(firstList.length)];
     final last = lastList[_random.nextInt(lastList.length)];
     return '$first $last';
-  }
-
-  static String _randomSexuality() {
-    final roll = _random.nextInt(100);
-    if (roll < 85) return 'Heteroseksual';
-    if (roll < 93) return 'Biseksual';
-    return 'Homoseksual';
   }
 
   static void generateClassmatesIfEmpty(Character character) {
@@ -66,7 +29,7 @@ class SchoolGenerator {
         'relationship': (40 + _random.nextInt(21)).toString(), // 40 to 60 initial
         'age': character.age.toString(),
         'isDeceased': 'false',
-        'sexuality': _randomSexuality(),
+        'sexuality': SexualityLogic.getStudentSexuality(gender),
       });
     }
   }
@@ -80,7 +43,7 @@ class SchoolGenerator {
         'gender': gender,
         'relationship': (40 + _random.nextInt(21)).toString(),
         'age': (35 + _random.nextInt(26)).toString(), // 35 to 60
-        'sexuality': _randomSexuality(),
+        'sexuality': SexualityLogic.getTeacherSexuality(gender),
       };
     }
 
@@ -92,7 +55,7 @@ class SchoolGenerator {
         'gender': gender,
         'relationship': (40 + _random.nextInt(21)).toString(),
         'age': (28 + _random.nextInt(23)).toString(), // 28 to 50
-        'sexuality': _randomSexuality(),
+        'sexuality': SexualityLogic.getTeacherSexuality(gender),
       };
     }
 
@@ -106,7 +69,7 @@ class SchoolGenerator {
           'relationship': (45 + _random.nextInt(16)).toString(),
           'subject': ['Matematika', 'B. Indonesia', 'IPA', 'IPS', 'B. Inggris'][i % 5],
           'age': (25 + _random.nextInt(31)).toString(),
-          'sexuality': _randomSexuality(),
+          'sexuality': SexualityLogic.getTeacherSexuality(gender),
         });
       }
     }
@@ -121,7 +84,7 @@ class SchoolGenerator {
           'relationship': (45 + _random.nextInt(16)).toString(),
           'subject': ['Matematika', 'B. Indonesia', 'Fisika', 'Sejarah', 'Olahraga'][i % 5],
           'age': (25 + _random.nextInt(31)).toString(),
-          'sexuality': _randomSexuality(),
+          'sexuality': SexualityLogic.getTeacherSexuality(gender),
         });
       }
     }
@@ -136,7 +99,7 @@ class SchoolGenerator {
           'relationship': (45 + _random.nextInt(16)).toString(),
           'subject': ['Kalkulus', 'Kimia', 'Biologi', 'Sosiologi', 'Ekonomi'][i % 5],
           'age': (25 + _random.nextInt(31)).toString(),
-          'sexuality': _randomSexuality(),
+          'sexuality': SexualityLogic.getTeacherSexuality(gender),
         });
       }
     }
