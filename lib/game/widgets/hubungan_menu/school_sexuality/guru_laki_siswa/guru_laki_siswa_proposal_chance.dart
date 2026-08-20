5df3993f9b56f239ddfc8129a5c24258f6ab79fe
Muhfahmm <fahimfahim0407@gmail.com>
2026-08-20 20:03:30 +0700

    add and register gay/lesbian proposal chance configuration files

---
+++ lib/game/widgets/hubungan_menu/school_sexuality/guru_laki_siswa/guru_laki_siswa_proposal_chance.dart
@@ -0,0 +1,33 @@
+// lib/game/widgets/hubungan_menu/school_sexuality/guru_laki_siswa/guru_laki_siswa_proposal_chance.dart
+
+class GuruLakiSiswaProposalChance {
+  /// Mendapatkan persentase ajakan pacaran dari Guru Laki-laki ke Siswa Laki-laki (Gay) berdasarkan usia
+  static int getPacaranChance(int age) {
+    if (age == 6) return 5;
+    if (age == 7) return 10;
+    if (age == 8) return 15;
+    if (age == 9) return 20;
+    if (age == 10) return 25;
+    if (age == 11) return 30;
+    if (age == 12) return 35;
+    if (age == 13) return 40;
+    if (age == 14) return 45;
+    if (age >= 15) return 50;
+    return 0;
+  }
+
+  /// Mendapatkan persentase ajakan berhubungan intim dari Guru Laki-laki ke Siswa Laki-laki (Gay) berdasarkan usia
+  static int getBercintaChance(int age) {
+    if (age == 6) return 3;
+    if (age == 7) return 7;
+    if (age == 8) return 15;
+    if (age == 9) return 18;
+    if (age == 10) return 23;
+    if (age == 11) return 30;
+    if (age == 12) return 35;
+    if (age == 13) return 40;
+    if (age == 14) return 45;
+    if (age >= 15) return 50;
+    return 0;
+  }
+}
