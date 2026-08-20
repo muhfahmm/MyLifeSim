7d21b1187613e1985949bfa0cb8e96497da2a4e7
Muhfahmm <fahimfahim0407@gmail.com>
2026-08-20 19:58:48 +0700

    restore original proposal chances for all files

--- lib/game/widgets/hubungan_menu/school_sexuality/guru_perempuan_siswa/guru_perempuan_proposal_chance.dart
+++ lib/game/widgets/hubungan_menu/school_sexuality/guru_perempuan_siswa/guru_perempuan_proposal_chance.dart
@@ -3,11 +3,31 @@
 class GuruPerempuanProposalChance {
   /// Mendapatkan persentase ajakan pacaran dari Guru Perempuan ke Siswa Laki-laki berdasarkan usia
   static int getPacaranChance(int age) {
-    return 100;
+    if (age == 6) return 5;
+    if (age == 7) return 10;
+    if (age == 8) return 15;
+    if (age == 9) return 20;
+    if (age == 10) return 25;
+    if (age == 11) return 30;
+    if (age == 12) return 35;
+    if (age == 13) return 40;
+    if (age == 14) return 45;
+    if (age >= 15) return 50;
+    return 0;
   }
 
   /// Mendapatkan persentase ajakan berhubungan intim dari Guru Perempuan ke Siswa Laki-laki berdasarkan usia
   static int getBercintaChance(int age) {
-    return 100;
+    if (age == 6) return 3;
+    if (age == 7) return 7;
+    if (age == 8) return 15;
+    if (age == 9) return 18;
+    if (age == 10) return 23;
+    if (age == 11) return 30;
+    if (age == 12) return 35;
+    if (age == 13) return 40;
+    if (age == 14) return 45;
+    if (age >= 15) return 50;
+    return 0;
   }
 }
