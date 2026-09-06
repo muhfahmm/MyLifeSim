ae2155aed3f55d1abb95790c0e86c25b7582b66f
muhfahmm <fahimfahim0407@gmail.com>
2026-09-06 19:45:24 +0700

    update tes seleksi

--- lib/game/widgets/aktivitas_menu/pilih_aktivitas/pendidikan_karir/univ_logic/unniv_negeri_tes_seleksi/bisnis_ekonomi/bisnis_ekonomi_resolver.dart
+++ lib/game/widgets/aktivitas_menu/pilih_aktivitas/pendidikan_karir/univ_logic/unniv_negeri_tes_seleksi/bisnis_ekonomi/bisnis_ekonomi_resolver.dart
@@ -1,11 +1,20 @@
 import '../tes_seleksi_model.dart';
 import 'tes_manajemen.dart';
 import 'tes_akuntansi.dart';
+import 'tes_pemasaran_digital.dart';
+import 'tes_ekonomi_pembangunan.dart';
+import 'tes_perbankan_keuangan.dart';
 
 List<QuestionItem> getBisnisEkonomiQuestions(String major) {
   final String m = major.toLowerCase();
   if (m.contains('akuntansi')) {
     return getAkuntansiQuestions();
+  } else if (m.contains('pemasaran')) {
+    return getPemasaranDigitalQuestions();
+  } else if (m.contains('pembangunan') || m.contains('pembangunan ekonomi')) {
+    return getEkonomiPembangunanQuestions();
+  } else if (m.contains('bank') || m.contains('keuangan')) {
+    return getPerbankanKeuanganQuestions();
   } else {
     return getManajemenQuestions();
   }
