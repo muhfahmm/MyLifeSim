7371f4a3069f86fd0eaa4e16b81926c380c38bde
muhfahmm <fahimfahim0407@gmail.com>
2026-09-06 20:18:42 +0700

    update tes seleksi

--- lib/game/widgets/aktivitas_menu/pilih_aktivitas/pendidikan_karir/univ_logic/unniv_negeri_tes_seleksi/hukum_sosial/hukum_sosial_resolver.dart
+++ lib/game/widgets/aktivitas_menu/pilih_aktivitas/pendidikan_karir/univ_logic/unniv_negeri_tes_seleksi/hukum_sosial/hukum_sosial_resolver.dart
@@ -1,11 +1,23 @@
 import '../tes_seleksi_model.dart';
 import 'tes_hukum.dart';
 import 'tes_psikologi.dart';
+import 'tes_hubungan_internasional.dart';
+import 'tes_ilmu_komunikasi.dart';
+import 'tes_administrasi_publik.dart';
+import 'tes_kriminologi.dart';
 
 List<QuestionItem> getHukumSosialQuestions(String major) {
   final String m = major.toLowerCase();
   if (m.contains('psikologi')) {
     return getPsikologiQuestions();
+  } else if (m.contains('hubungan internasional') || m.contains('hi')) {
+    return getHubunganInternasionalQuestions();
+  } else if (m.contains('komunikasi')) {
+    return getIlmuKomunikasiQuestions();
+  } else if (m.contains('administrasi publik') || m.contains('negara')) {
+    return getAdministrasiPublikQuestions();
+  } else if (m.contains('kriminologi')) {
+    return getKriminologiQuestions();
   } else {
     return getHukumQuestions();
   }
